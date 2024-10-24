a7b6368b71016a9f4f249997b335a3b96cb89414
Anna Vo <avo1@ufl.edu>
2024-10-23 21:36:36 -0400

    uncommented and removed code in main

--- encoder.py
+++ encoder.py
@@ -31,10 +31,8 @@ def main():
             print("Your password has been encoded and stored!\n")
         # If user selects option 2, decode the stored encoded password
         elif user_choice == "2":
-            # *** Once the decode function is written, the following 2 lines should be uncommented and the pass command should be removed ***
-            # decoded_password = decode(encoded_password)
-            # print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.\n")
-            pass
+            decoded_password = decode(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.\n")
         # If user selects option 3, exit the program loop
         elif user_choice == "3":
             program_continue = False
